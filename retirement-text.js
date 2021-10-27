e8f64ed92328d0ce60026fcc7d0b4f5c3f4969d2
Los <cmuro10@gmail.com>
2021-10-26 22:23:55 -0600

    Update retirement calculator

---
+++ retirement-text.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const retirementText = {
+    1: 'you need to start saving more money.',
+    2: 'not bad, but you can do better.',
+    3: "you're doing pretty good! But I think you can do better!",
+    4: "great, you're ahead of most people!",
+    5: "You're killing it!",
+    6: 'great work, retirement is right around the corner!',
+    7: "If you're not already retired, you'll retire any day now! You made it to the end-game, enjoy retirement!",
+};
+
+module.exports.retirementText = retirementText;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const PORT = 3000;
 
+const retirementText = require('./retirement-text').retirementText;
+
 const express = require('express');
 const app = express();
 
@@ -21,25 +23,88 @@ app.post('/investment_returns', (req, res) => {
 
     selection === 'weekly' ? (investmentValue = expense * 752) : (investmentValue = expense * 173);
 
-    console.log(investmentValue);
-
     res.send(
         `If you were to invest your ${selection} expense of $${expense}, your investment would total $${investmentValue} after 10 years.`
     );
 });
 
-app.post('/stock_search', (req, res) => {
+app.post('/retirement', (req, res) => {
     const request = req.body;
-    const selection = request.optradio;
+    const income = parseInt(request.income);
+    const expense = parseInt(request.expense);
 
-    const findStockByPrice = (selection) => {
-        const maxCost = stocks.reduce((prev, curr) => (prev.price > curr.price ? prev : curr));
-        const minCost = stocks.reduce((prev, curr) => (prev.price < curr.price ? prev : curr));
+    if (expense > income) {
+        res.send("Your spend is greater than your income, you'll never retire at this rate.");
+    } else {
+        const savingsPercent = parseInt(100 - (expense / income) * 100);
+        const initialText = "You'll retire in";
+        let text;
 
-        return selection === 'Max' ? maxCost : minCost;
-    };
+        switch (true) {
+            case savingsPercent <= 5:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 66 years, ${retirementText[1]}`;
+                break;
+            case savingsPercent <= 10:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 51 to 66 years, ${retirementText[1]}`;
+                break;
+            case savingsPercent <= 15:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 43 to 50 years, ${retirementText[1]}`;
+                break;
+            case savingsPercent <= 20:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 37 to 42 years, ${retirementText[1]}`;
+                break;
+            case savingsPercent <= 25:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 32 to 36 years, ${retirementText[1]}`;
+                break;
+            case savingsPercent <= 30:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 28 to 31 years, ${retirementText[1]}`;
+                break;
+            case savingsPercent <= 35:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 25 to 27 years, ${retirementText[2]}`;
+                break;
+            case savingsPercent <= 40:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 22 to 24 years, ${retirementText[2]}`;
+                break;
+            case savingsPercent <= 45:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 19 to 21 years, ${retirementText[2]}`;
+                break;
+            case savingsPercent <= 50:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 17 to 18 years, ${retirementText[2]}`;
+                break;
+            case savingsPercent <= 55:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 14.5 to 16 years, ${retirementText[3]}`;
+                break;
+            case savingsPercent <= 60:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 12.5 to 14.5 years, ${retirementText[3]}`;
+                break;
+            case savingsPercent <= 65:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 10.5 to 12.5 years, ${retirementText[3]}`;
+                break;
+            case savingsPercent <= 70:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 8.5 to 10.5 years, ${retirementText[4]}`;
+                break;
+            case savingsPercent <= 75:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 7 to 8.5 years, ${retirementText[4]}`;
+                break;
+            case savingsPercent <= 80:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 5.5 to 7 years, ${retirementText[4]}`;
+                break;
+            case savingsPercent <= 85:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 4 to 5.5 years, ${retirementText[5]}`;
+                break;
+            case savingsPercent <= 90:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 3 to 4 years, ${retirementText[6]}`;
+                break;
+            case savingsPercent <= 95:
+                text = `Your savings rate is ${savingsPercent}%. ${initialText} 2 to 3 years, ${retirementText[6]}`;
+                break;
+            case savingsPercent <= 100:
+                text = `Your savings rate is ${savingsPercent}%. ${retirementText[7]}`;
+                break;
+        }
 
-    res.send(findStockByPrice(selection));
+        res.send(text);
+    }
 });
 
 app.listen(PORT, () => {
